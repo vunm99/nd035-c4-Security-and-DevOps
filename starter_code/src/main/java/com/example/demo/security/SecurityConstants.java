de56b22b1d147b8151ab6b01b124aa4014daa19d
Nguyễn Minh Vũ <43573300+vunm99@users.noreply.github.com>
2023-10-24 22:00:58 +0700

    JWTAuthentication
    JWTAuthentication

---
+++ starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -0,0 +1,20 @@
+package com.example.demo.security;
+
+public class SecurityConstants {
+
+	public static final String SECRET = "oursecretkey";
+    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/api/user/create";
+    public static final String LOGIN_URL  = "/login";
+
+    public static final String SUCCESS  = "SUCCESS";
+
+    public static final String FAIL  = "FAIL";
+
+    public static final String LOG_FORMAT = "Method: %s, Status: %s - Request: %s";
+
+
+
+}
